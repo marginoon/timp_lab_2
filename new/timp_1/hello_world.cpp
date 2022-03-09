33dd14bcad880648b77b188254ccb27dbd922adc
marginoon <g-l-o-120404@rambler.ru>
2022-03-09 14:21:53 -0500

    deleting rubbish for 3 point

--- new/timp_1/hello_world.cpp
+++ new/timp_1/hello_world.cpp
@@ -4,15 +4,6 @@
 
 int main(){
     std::string name; // comments
-    std::cin >> name; // comments
+    std::cin >> name; // comments f
     std::cout << "Hello world from" << name << std::endl; // comments
-}#include <iostream>
-#include <string>
-
-int
-main()
-{
-  std::string name;                                     // comments
-  std::cin >> name;                                     // comments
-  std::cout << "Hello world from" << name << std::endl; // comments
 }
