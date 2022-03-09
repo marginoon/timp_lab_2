231fa0480b869cb85acce1cd921c9b720d061f17
marginoon <g-l-o-120404@rambler.ru>
2022-03-09 14:00:15 -0500

    use clang

--- new/timp_1/hello_world.cpp
+++ new/timp_1/hello_world.cpp
@@ -6,4 +6,13 @@ int main(){
     std::string name; // comments
     std::cin >> name; // comments
     std::cout << "Hello world from" << name << std::endl; // comments
+}#include <iostream>
+#include <string>
+
+int
+main()
+{
+  std::string name;                                     // comments
+  std::cin >> name;                                     // comments
+  std::cout << "Hello world from" << name << std::endl; // comments
 }
