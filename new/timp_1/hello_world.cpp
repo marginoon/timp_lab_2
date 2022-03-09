3fdc3ea7cc6e5c9eede1302ed5114f6191d3579c
marginoon <g-l-o-120404@rambler.ru>
2022-03-09 13:40:16 -0500

    adding new commits and including string

--- new/timp_1/hello_world.cpp
+++ new/timp_1/hello_world.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <string>
 
 
 int main(){
-    std::string name;
-    std::cin >> name;
-    std::cout << "Hello world from" << name << std::endl;
+    std::string name; //
+    std::cin >> name; //
+    std::cout << "Hello world from" << name << std::endl; //
 }
